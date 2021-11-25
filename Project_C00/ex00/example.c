9f88d2956f776559dcdbf0c1e198a7e2eb61a593
Zac <mzorigt91@gmail.com>
2021-11-25 15:48:47 +1030

    commit

--- Project_C00/ex00/example.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-void function1(){  
-int x=10;//local variable  
-static int y=10;//static variable  
-x=x+1;  
-y=y+1;  
-printf("%d,%d",x,y);  
-} 
--- Project_C00/ex00/ft_putchar.c
+++ Project_C00/ex00/ft_putchar.c
@@ -1,3 +1,4 @@
+
 #include <unistd.h>
 
 void ft_putchar(char c)
