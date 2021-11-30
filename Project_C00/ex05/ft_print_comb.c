2bf9a49eb4d9dd15652e5e8bd31952577670972a
Zac0124 <mzorigt91@gmail.com>
2021-11-30 13:04:29 +1030

    upload

--- Project_C00/ex05/ft_print_comb.c
+++ Project_C00/ex05/ft_print_comb.c
@@ -1,26 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zmunkhja <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/29 14:45:39 by zmunkhja          #+#    #+#             */
-/*   Updated: 2021/11/29 14:48:07 by zmunkhja         ###   ########.fr       */
+/*   Created: 2021/11/30 11:04:07 by zmunkhja          #+#    #+#             */
+/*   Updated: 2021/11/30 11:04:15 by zmunkhja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
 #include <unistd.h>
 
-void ft_putchar(char c)
+void ft_print_comb(void)
 {
-	write(1, &c, 1);
-}
 
+	char a;		
+	char b;
+	char c;
+
+	a = '0';
+
+	while ( a<='7')
+	{ b = a+1;
+		while (b<='8')
+
+		{ c = b+1;
+			while (c <= '9')
+
+			{	write ( 1,&a,1);
+				write (1,&b,1);
+				write (1,&c,1);
+
+				if (!(a=='7' && b=='8' && c=='9'))
+					write (1,", ",2);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+
+}
 int main(void)
-{
-	ft_putchar('a');
-	return (0);
+
+{ ft_print_comb();
 }
